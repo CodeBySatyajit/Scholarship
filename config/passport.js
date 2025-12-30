4a079aed914a643d744c8255388316043b262400
Satyajit <satyjeetgaikwad39@gmail.com>
2025-12-30 10:53:51 +0530

    fix error for google auth system

--- config/passport.js
+++ config/passport.js
@@ -56,7 +56,7 @@ passport.use('google', new GoogleStrategy(
   {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:3000/auth/google/callback'
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
